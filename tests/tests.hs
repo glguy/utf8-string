b486d5c5cb8fe53adde8347f4abdc896c00c9abc
emertens <emertens@galois.com>
2007-07-06 07:17:11 -0700

    tests/tests.hs: updates tests to compile
    darcs-hash:20070706141711-7e720-8ccf4cee542b7046bf254c84e551526742b26daa.gz

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,8 +1,10 @@
+import Codec.Binary.UTF8.String
+
 main = do
-  putStrLn $ "Encode Decode: " ++ show encodeDecodeTest
+  putStrLn $ "Encode Decode failures: " ++ show encodeDecodeTest
   
-encodeDecodeTest = all (\x -> x == decode (encode x)) legal_codepoints
-                && all (\x -> '\xfffd' == decode (encode x)) illegal_codepoints
+encodeDecodeTest = filter (\x -> [x] /= decode (encode [x])) legal_codepoints ++
+                   filter (\x -> ['\xfffd'] /= decode (encode [x])) illegal_codepoints
   where
     legal_codepoints = ['\0'..'\xd7ff'] ++ ['\xe000'..'\xfffd'] ++ ['\x10000'..'\x10ffff']
-    illegal_codepoints = '\xffff' : '\xfffe' : ['\xd800'..'\xdfff'] ++ ['\x10000'..'\x10ffff']
+    illegal_codepoints = '\xffff' : '\xfffe' : ['\xd800'..'\xdfff']
