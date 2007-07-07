9d1abdfd79ecfa64e077334ea9c14d4879bea025
Don Stewart <dons@cse.unsw.edu.au>
2007-07-06 18:36:19 -0700

    tidy up test suite a bit
    darcs-hash:20070707013619-9c5c1-069d61d38c1b3beae73d68dbf69d1eb598dbd2d8.gz

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -1,8 +1,14 @@
 import Codec.Binary.UTF8.String
 
-main = do
-  putStrLn $ "Encode Decode failures: " ++ show encodeDecodeTest
-  
+main = let v = encodeDecodeTest
+       in putStrLn $ case v of
+            [] -> "Ok. All passed."
+            s  -> "Encode/decode failures: " ++ show s
+
+--
+-- test decode . encode == id for the class of chars we know that to be true of
+--
+encodeDecodeTest :: [Char]
 encodeDecodeTest = filter (\x -> [x] /= decode (encode [x])) legal_codepoints ++
                    filter (\x -> ['\xfffd'] /= decode (encode [x])) illegal_codepoints
   where
