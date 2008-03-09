5ec6d2885caef8ee406637ed1e1ac2387b37b01b
iavor.diatchki <iavor.diatchki@gmail.com>
2008-03-08 19:19:01 -0800

    Clean up test, provide a temp. solution to the "out of tsuck" issue
    It appears that the "out of stack" issue is called by one of the rules.
    My suspicion is that the rules is "fold/augment", which is used by (++).
    For the moment I used a custom (++) function, which makes things work it seems.
    
    darcs-hash:20080309031901-5035a-2a2ecfd349e50566640a7d5aef3e8db4602888d0.gz

--- tests/BenchBytestring.hs
+++ tests/BenchBytestring.hs
@@ -1,23 +1,44 @@
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as Char8
-import qualified Data.ByteString.UTF8 as UTF8
-import qualified Codec.Binary.UTF8.String as List
+import qualified Data.ByteString      as S
+import qualified Data.ByteString.Lazy as L
+import qualified Data.String.UTF8     as UTF8
 
-main = main3
+import System.IO
+import Data.Word
 
-main1 = do txt <- BS.readFile "test"
-           print (UTF8.length txt)
+main  = main4
 
-main2 = do txt <- readFile "test"
-           print (length $ List.decodeString txt)
+main1 = do putStrLn "Speed: Data.ByteString"
+           txt <- S.readFile "test"
+           print (UTF8.length $ UTF8.fromRep txt)
 
-main3 = print encodeDecodeTest
+main2 = do putStrLn "Speed: Data.ByteString.Lazy"
+           txt <- L.readFile "test"
+           print (UTF8.length $ UTF8.fromRep txt)
 
-encodeDecodeTest :: (String,String)
-encodeDecodeTest = (filter (\x -> [x] /= f x) legal_codepoints,
-                   filter (\x -> ['\xfffd'] /= f x) illegal_codepoints)
+main3 = do putStrLn "Speed: [Word8]"
+           txt <- hGetContents =<< openBinaryFile "test" ReadMode
+           let bytes :: [Word8]
+               bytes = map (fromIntegral . fromEnum) txt
+           print (UTF8.length $ UTF8.fromRep bytes)
+
+main4 = do putStrLn "Correctness: Data.ByteString"
+           print (encodeDecodeTest enc)
+
+app [] ys = ys
+app (x:xs) ys = x : app xs ys
+
+infixr `app`
+
+encodeDecodeTest :: (Char -> String) -> String
+encodeDecodeTest f =
+     filter (\x -> [x] /= f x) legal_codepoints
+  ++ filter (\x -> [UTF8.replacement_char] /= f x) illegal_codepoints
   where
-    legal_codepoints = ['\0'..'\xd7ff'] ++ ['\xe000'..'\xfffd'] ++ ['\x10000'..'\x10ffff']
-    illegal_codepoints = '\xffff' : '\xfffe' : ['\xd800'..'\xdfff']
+    legal_codepoints    = ['\0'..'\xd7ff'] `app` ['\xe000'..'\xfffd']
+                       `app` ['\x10000'..'\x10ffff']
+    illegal_codepoints  = '\xffff' : '\xfffe' : ['\xd800'..'\xdfff']
 
-    f x = UTF8.toString (UTF8.fromString [x])
+
+enc x = UTF8.toString (UTF8.fromString [x] :: UTF8.UTF8 S.ByteString)
+
+
