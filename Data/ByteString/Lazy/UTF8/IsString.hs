ee00bac34abc642f835b179bae908d0f9ddbb05c
Krzysztof Skrzętnicki <gtener@gmail.com>
2011-04-09 01:48:10 +0200

    use newtype for IsString instance to avoid orphan instances and problems that they cause

--- Data/ByteString/Lazy/UTF8/IsString.hs
+++ Data/ByteString/Lazy/UTF8/IsString.hs
@@ -1,9 +1,11 @@
-module Data.ByteString.Lazy.UTF8.IsString () where
+module Data.ByteString.Lazy.UTF8.IsString (UTF8Lit(..)) where
 
 import Data.ByteString.Lazy as B
 import Data.ByteString.Lazy.UTF8 as B8
 import Data.String
 
-instance IsString ByteString where
+newtype UTF8Lit = UTF8Lit { litUTF8 :: ByteString }
+
+instance IsString UTF8Lit where
     {-# INLINE fromString #-}
-    fromString = B8.fromString
+    fromString = UTF8Lit . B8.fromString
